af7fd8e87eee4960bdf5403a9a14c4b9e57b4b02
shi zhangfan <shijiangfan@qq.com>
2019-05-14 22:02:30 +0800

    自动任务10%

--- insboot/ins/tasks.py
+++ insboot/ins/tasks.py
@@ -1,7 +1,14 @@
 from celery.task import task
+from .models import Account
 
 
 @task
 def print_hello():
+    print("hello celery and django...")
     return "hello celery and django..."
+
+
+@task
+def follow_worker():
+    # 获取所有账号
 
